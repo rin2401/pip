486a5b401c9fd22dc7508c10755ff01f1e51cc4e
rin2401 <rin2401@gmail.com>
2025-06-19 15:35:53 +0700

    Update and rename requirements.txt to install_torch_2.6.0.sh

---
+++ install_torch_2.6.0.sh
@@ -0,0 +1,4 @@
+pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 --index-url https://download.pytorch.org/whl/cu126
+pip install transformers==4.52.4
+pip install flash-attn==2.7.4.post1 --no-build-isolation
+pip install vllm==0.8.5.post1
