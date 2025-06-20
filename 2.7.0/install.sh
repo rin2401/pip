316119f047f0bbbac6fde231018b320b5e352324
rin2401 <rin2401@gmail.com>
2025-06-20 04:12:45 +0000

    Add torch v2.7.0

---
+++ 2.7.0/install.sh
@@ -0,0 +1,4 @@
+pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 --index-url https://download.pytorch.org/whl/cu126
+pip install transformers==4.52.4
+pip install flash-attn==2.8.0.post2 --no-build-isolation
+pip install vllm==0.9.1
