58b72b6267cbb17dee2c9a1a4b256c05e6761035
rin2401 <rin2401@gmail.com>
2025-09-29 17:58:25 +0700

    Update install.sh

--- 2.7.0/install.sh
+++ 2.7.0/install.sh
@@ -1,4 +1,11 @@
-pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 --index-url https://download.pytorch.org/whl/cu126
-pip install transformers==4.52.4
-pip install flash-attn==2.8.0.post2 --no-build-isolation
-pip install vllm==0.9.1
+pip install torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1 --index-url https://download.pytorch.org/whl/cu126
+pip install transformers==4.56.2
+pip install vllm==0.10.0
+
+# pip install flash-attn==2.8.0.post2 --no-build-isolation
+
+# PY="cp310"
+# PY="cp312"
+PY="cp311"
+
+pip install https://github.com/Dao-AILab/flash-attention/releases/download/v2.7.4.post1/flash_attn-2.7.4.post1+cu12torch2.7cxx11abiFALSE-$PY-$PY-linux_x86_64.whl  --no-build-isolation
