911110ec5be4313b757d9de605507d9318b0a3b4
rin2401 <rin2401@gmail.com>
2026-04-16 12:04:02 +0700

    Update flash-attention wheel version in install.sh

--- 2.9.0/install.sh
+++ 2.9.0/install.sh
@@ -8,7 +8,7 @@ pip install torch==2.9.1 torchvision==0.24.1 torchaudio==2.9.1 --index-url https
 # PY="cp312"
 PY="cp311"
 
-# pip install --no-build-isolation https://github.com/Dao-AILab/flash-attention/releases/download/v2.7.4.post1/flash_attn-2.7.4.post1+cu12torch2.7cxx11abiFALSE-cp311-cp311-linux_x86_64.whl  
+pip install --no-build-isolation https://github.com/Dao-AILab/flash-attention/releases/download/v2.8.3/flash_attn-2.8.3+cu12torch2.9cxx11abiTRUE-cp312-cp312-linux_x86_64.whl
 
 # mamba
 pip install --no-build-isolation https://github.com/state-spaces/mamba/releases/download/v2.3.1/mamba_ssm-2.3.1+cu12torch2.9cxx11abiTRUE-cp311-cp311-linux_x86_64.whl
