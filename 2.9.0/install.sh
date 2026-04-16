33a38f630d8c83a8e8d742a2049b9fc3eb3c9c1b
rin2401 <rin2401@gmail.com>
2026-04-16 04:29:44 +0000

    Add install.sh for PyTorch and related packages

---
+++ 2.9.0/install.sh
@@ -0,0 +1,15 @@
+pip install torch==2.9.1 torchvision==0.24.1 torchaudio==2.9.1 --index-url https://download.pytorch.org/whl/cu128
+# pip install transformers==4.56.2
+# pip install vllm==0.10.0
+
+# pip install flash-attn==2.8.0.post2 --no-build-isolation
+
+# PY="cp310"
+# PY="cp312"
+PY="cp311"
+
+# pip install --no-build-isolation https://github.com/Dao-AILab/flash-attention/releases/download/v2.7.4.post1/flash_attn-2.7.4.post1+cu12torch2.7cxx11abiFALSE-cp311-cp311-linux_x86_64.whl  
+
+# mamba
+pip install --no-build-isolation https://github.com/state-spaces/mamba/releases/download/v2.3.1/mamba_ssm-2.3.1+cu12torch2.9cxx11abiTRUE-cp311-cp311-linux_x86_64.whl
+pip install --no-build-isolation https://github.com/Dao-AILab/causal-conv1d/releases/download/v1.6.1.post4/causal_conv1d-1.6.1+cu12torch2.9cxx11abiTRUE-cp311-cp311-linux_x86_64.whl
